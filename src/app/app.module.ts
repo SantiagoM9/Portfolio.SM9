ff894fdad5932d4cf324da5d96c06e5dad8a09c1
SantiagoM9 <santiago.musante@hotmail.com>
2022-08-20 20:41:12 -0300

    2do dia de modificaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeMiComponent } from './components/acerca-de-mi/acerca-de-mi.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeMiComponent  
   ],
   imports: [
     BrowserModule,
